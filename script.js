b41e5c7e57398855a419dd1e735f531fa578a228
Willian Matheus Ribeiro Tesche <85637800+williamtesche@users.noreply.github.com>
2021-11-20 12:31:29 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password]');
+
+window.onload = init;
